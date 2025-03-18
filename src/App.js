59f583a7ba5c6a293225f3e412922abdde9ce2dd
Sergey Arefkin <arefkins@globalbit.co.il>
2025-03-18 13:39:48 +0400

    app init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import {checkbox} from '@igds/react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+{/* here i get the 194 errors, if i comment the usage of Checkbox, the errors are gone*/}
+      <checkbox></checkbox>
+       
+    </div>
+  );
+}
+
+export default App;
